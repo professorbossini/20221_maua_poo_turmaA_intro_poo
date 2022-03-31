3a2d428cc1bd21c683527a7d61b30f5cb7419e2b
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-31 11:09:03 -0300

    Mais exemplos com variaveis e objetos. Retomar falando de construtores

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -2,11 +2,25 @@
 public class LivroDeNotas{
     //variável de instância
     //instância é sinônimo de objeto
-    String nomeDaDisciplina;
+    //modificador de acesso: private
+    private String nomeDisciplina;
+
+    //método modificador
+    //setter
+    public void setNomeDisciplina(String nomeDisciplina){
+        //operador de auto referência: this
+        this.nomeDisciplina = nomeDisciplina;
+    }
+
+    //método de acesso
+    //getter
+    public String getNomeDisciplina(){
+        return nomeDisciplina;
+    }
 
 
     //método
     public void exibirMensagem(){
-        System.out.println ("Bem vindo ao livro de notas da disciplina " + nomeDaDisciplina);    
+        System.out.println ("Bem vindo ao livro de notas da disciplina " + nomeDisciplina);    
     } 
 }
